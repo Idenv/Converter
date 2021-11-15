7c08d270b3abb9281e8d9b77d1213c1c8c07892d
Vlad Teterev <vladtett2000@gmail.com>
2021-11-15 10:57:58 +0100

    initial commit

---
+++ Converter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Converter
+{
+    class Converter
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Converter");
+        }
+    }
+}
